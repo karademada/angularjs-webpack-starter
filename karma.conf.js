46513a240a01b46218d1dec3d0a7ec69cbe1ff01
Sebastien <seb@dsebastien.net>
2016-02-05 02:15:32 +0100

    Updated karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,71 +1,87 @@
-// @AngularClass
-var path = require('path');
+// Karma configuration
+// reference: http://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = function(config) {
-  var testWebpackConfig = require('./webpack.test.config.js');
-  config.set({
+module.exports = function (config) {
+	var testWebpackConfig = require("./webpack.test.config.js");
+	config.set({
 
-    // base path that will be used to resolve all patterns (e.g. files, exclude)
-    basePath: '',
+		// base path that will be used to resolve all patterns (e.g. files, exclude)
+		basePath: "",
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+		frameworks: [
+			"jasmine"
+		],
 
-    // list of files to exclude
-    exclude: [ ],
+		// list of files / patterns to load in the browser
+		files: [{pattern: "spec-bundle.js", watched: false}],
 
-    // list of files / patterns to load in the browser
-    // we are building the test environment in ./spec-bundle.js
-    files: [ { pattern: 'spec-bundle.js', watched: false } ],
+		// preprocess matching files before serving them to the browser
+		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+		preprocessors: {"spec-bundle.js": ["coverage", "webpack", "sourcemap"]},
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: { 'spec-bundle.js': ['coverage', 'webpack', 'sourcemap'] },
+		// list of files to exclude
+		exclude: [],
 
-    // Webpack Config at ./webpack.test.config.js
-    webpack: testWebpackConfig,
+		// list of paths mappings
+		// can be used to map paths served by the Karma web server to /base/ content
+		// knowing that /base corresponds to the project root folder (i.e., where this config file is located)
+		proxies: {},
 
-    coverageReporter: {
-      dir : 'coverage/',
-      reporters: [
-        { type: 'text-summary' },
-        { type: 'json' },
-        { type: 'html' }
-      ]
-    },
 
-    // Webpack please don't spam the console when running in karma!
-    webpackServer: { noInfo: true },
+		// Webpack Config at ./webpack.test.config.js
+		webpack: testWebpackConfig,
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: [ 'mocha', 'coverage' ],
+		// test coverage
+		coverageReporter: {
+			dir: "coverage/",
+			reporters: [
+				{type: "text-summary"},
+				{type: "json"},
+				{type: "html"}
+			]
+		},
 
-    // web server port
-    port: 9876,
+		// Webpack please don"t spam the console when running in karma!
+		webpackServer: {noInfo: true},
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+		// test results reporter to use
+		// possible values: "dots", "progress", "spec", "junit"
+		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
+		// https://www.npmjs.com/package/karma-junit-reporter
+		// https://www.npmjs.com/package/karma-spec-reporter
+		reporters: ["spec", "progress", "coverage"],
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+		// web server port
+		port: 9876,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [
-      // 'Chrome',
-      'PhantomJS'
-    ],
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  });
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
 
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: [
+			"PhantomJS"
+			//"Chrome",
+			//"Firefox",
+			//"IE"
+		],
+
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: true,
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: false,
+
+		junitReporter: {
+			outputFile: "target/reports/tests-unit/unit.xml",
+			suite: "unit"
+		},
+	});
 };

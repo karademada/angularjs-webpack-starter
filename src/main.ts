7f0d8f24524430e360fda0555e70a7a7796ca36e
Sebastien <seb@dsebastien.net>
2016-02-05 02:14:56 +0100

    Updated the main entrypoint

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,19 @@
-/*
- * Providers provided by Angular
- */
-import {provide, enableProdMode} from 'angular2/core';
-import {bootstrap, ELEMENT_PROBE_PROVIDERS} from 'angular2/platform/browser';
-import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
-import {HTTP_PROVIDERS} from 'angular2/http';
-
-const ENV_PROVIDERS = [];
-
-if ('production' === process.env.ENV) {
-  enableProdMode();
-} else {
-  ENV_PROVIDERS.push(ELEMENT_PROBE_PROVIDERS);
-}
+"use strict";
 
 /*
  * App Component
  * our top level component that holds all of our components
  */
-import {App} from './app/app';
+import {App} from "./app/app";
+
+// Import application styles
+import "./app/css/main.scss";
 
 /*
  * Bootstrap our Angular app with a top level component `App` and inject
- * our Services and Providers into Angular's dependency injection
+ * our Services and Providers into Angular's dependency injection system
  */
-document.addEventListener('DOMContentLoaded', function main() {
-  bootstrap(App, [
-    ...ENV_PROVIDERS,
-    ...HTTP_PROVIDERS,
-    ...ROUTER_PROVIDERS,
-    provide(LocationStrategy, { useClass: HashLocationStrategy })
-  ])
-  .catch(err => console.error(err));
-
+document.addEventListener("DOMContentLoaded", function main() {
+	console.log("Bootstrapping the App");
+	App.bootstrap();
 });
-
-// For vendors for example jQuery, Lodash, angular2-jwt just import them anywhere in your app
-// Also see custom_typings.d.ts as you also need to do `typings install x` where `x` is your module

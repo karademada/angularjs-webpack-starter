8a230506fe7c2d322b115552acce384216b70bce
Sebastien <seb@dsebastien.net>
2016-03-21 07:55:15 +0100

    Added docs

--- webpack.config.js
+++ webpack.config.js
@@ -27,16 +27,23 @@ const METADATA = {
 
 /*
  * Config
+ * See: http://webpack.github.io/docs/configuration.html#cli
  */
 module.exports = {
     // static data for index.html
     metadata: METADATA,
-
-    // reference: https://webpack.github.io/docs/configuration.html#devtool
-    devtool: "cheap-module-eval-source-map",
     
+    // reference: https://webpack.github.io/docs/configuration.html#devtool
+    // reference: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
+    devtool: "cheap-module-eval-source-map",
+
+    // Cache generated modules and chunks to improve performance for multiple incremental builds.
+    // Enabled by default in watch mode.
+    // You can pass false to disable it
+    // reference: http://webpack.github.io/docs/configuration.html#cache
     //cache: true,
     
+    // reference: http://webpack.github.io/docs/configuration.html#debug
     debug: true,
     
     stats: {
@@ -44,35 +51,61 @@ module.exports = {
         reasons: true
     },
 
-    // our angular app
+    // the entry point for the bundles
+    // reference: http://webpack.github.io/docs/configuration.html#entry
     entry: {
         "polyfills": helpers.root("src/polyfills.ts"),
         "vendor": helpers.root("src/vendor.ts"),
         "main": helpers.root("src/main.ts")
     },
 
-    // Config for our build files
-    // Adding hashes to files for cache busting
+    // Options affecting the output of the compilation
+    // reference: http://webpack.github.io/docs/configuration.html#output
+    
     output: {
         // Mandatory but not actually useful since everything remains in memory with webpack-dev-server
+        // reference: http://webpack.github.io/docs/configuration.html#output-path
         path: helpers.root("dist"),
         // We need to tell Webpack to serve our bundled application
         // from the build path. When proxying:
         // http://localhost:3000/ -> http://localhost:8080/
         publicPath: '/',
+        // Adding hashes to files for cache busting
+        // IMPORTANT: You must not specify an absolute path here!
+        // reference: http://webpack.github.io/docs/configuration.html#output-filename
         filename: "[name].[hash].bundle.js",
+        // The filename of the SourceMaps for the JavaScript files.
+        // They are inside the output.path directory.
+        //
+        // reference: http://webpack.github.io/docs/configuration.html#output-sourcemapfilename
         sourceMapFilename: "[name].[hash].map",
+        // The filename of non-entry chunks as relative path
+        // inside the output.path directory.
+        //
+        // reference: http://webpack.github.io/docs/configuration.html#output-chunkfilename
         chunkFilename: "[id].[hash].chunk.js"
     },
 
+    // Options affecting the resolving of modules.
+    //
+    // reference: http://webpack.github.io/docs/configuration.html#resolve
     resolve: {
+        // an array of extensions that should be used to resolve modules.
+        // reference: http://webpack.github.io/docs/configuration.html#resolve-extensions
         extensions: [ "", ".ts", ".js", ".json", ".css", ".scss", ".html" ]
     },
 
+    // Options affecting the normal modules.
+    //
+    // reference: http://webpack.github.io/docs/configuration.html#module
     module: {
         noParse: [
             // things that should not be parsed
         ],
+        
+        // An array of applied pre and post loaders.
+        //
+        // reference: http://webpack.github.io/docs/configuration.html#module-preloaders-module-postloaders
         preLoaders: [
             {
                 test: /\.ts$/,
@@ -89,8 +122,16 @@ module.exports = {
                 ]
             }
         ],
+
+        // An array of automatically applied loaders.
+        //
+        // IMPORTANT: The loaders here are resolved relative to the resource which they are applied to.
+        // This means they are not resolved relative to the configuration file.
+        //
+        // reference: http://webpack.github.io/docs/configuration.html#module-loaders
         loaders: [
             // Support for .ts files.
+            // reference: https://github.com/s-panferov/awesome-typescript-loader
             {
                 test: /\.ts$/,
                 loader: "awesome-typescript",
@@ -107,6 +148,7 @@ module.exports = {
             },
 
             // Support for CSS as raw text
+            // reference: https://github.com/webpack/raw-loader
             {
                 test: /\.css$/,
                 loader: "raw"
@@ -137,14 +179,31 @@ module.exports = {
         ]
     },
 
+    // Add additional plugins to the compiler.
+    //
+    // reference: http://webpack.github.io/docs/configuration.html#plugins
     plugins: [
+        // Plugin: ForkCheckerPlugin
+        // Description: Do type checking in a separate process, so webpack don't need to wait.
+        // reference: https://github.com/s-panferov/awesome-typescript-loader#forkchecker-boolean-defaultfalse
         new ForkCheckerPlugin(),
 
         // content hashes
         new WebpackSHAHash(),
 
         // optimization
+        // Plugin: OccurenceOrderPlugin
+        // Description: Varies the distribution of the ids to get the smallest id length
+        // for often used ids.
+        // reference: https://webpack.github.io/docs/list-of-plugins.html#occurrenceorderplugin
+        // reference: https://github.com/webpack/docs/wiki/optimization#minimize
         new webpack.optimize.OccurenceOrderPlugin(true),
+
+        // Plugin: CommonsChunkPlugin
+        // Description: Shares common code between the pages.
+        // It identifies common modules and put them into a commons chunk.
+        // reference: https://webpack.github.io/docs/list-of-plugins.html#commonschunkplugin
+        // reference: https://github.com/webpack/docs/wiki/optimization#multi-page-app
         new webpack.optimize.CommonsChunkPlugin({
             name: [ "main", "vendor", "polyfills" ],
             filename: "[name].[hash].bundle.js",
@@ -152,6 +211,10 @@ module.exports = {
         }),
 
         // static assets
+        // Plugin: CopyWebpackPlugin
+        // Description: Copy files and directories in webpack.
+        // Copies project static assets.
+        // reference: https://www.npmjs.com/package/copy-webpack-plugin
         new CopyWebpackPlugin([
             {
                 from: helpers.root("src/app/assets"),
@@ -170,20 +233,32 @@ module.exports = {
                 ]
             }
         ]),
-
-        // generating html
-        // Reference: https://github.com/ampedandwired/html-webpack-plugin
+        
+        // Plugin: HtmlWebpackPlugin
+        // Description: Simplifies creation of HTML files to serve your webpack bundles.
+        // This is especially useful for webpack bundles that include a hash in the filename
+        // which changes every compilation.
+        // reference: https://github.com/ampedandwired/html-webpack-plugin
         new HtmlWebpackPlugin({
             template: helpers.root("src/index.html"),
             chunksSortMode: "none"
         }),
 
         // Environment helpers (when adding more properties make sure you include them in environment.d.ts)
+        // Plugin: DefinePlugin
+        // Description: Define free variables.
+        // Useful for having development builds with debug logging or adding global constants.
+        // reference: https://webpack.github.io/docs/list-of-plugins.html#defineplugin
+        // NOTE: when adding more properties make sure you include them in custom-typings.d.ts
         new webpack.DefinePlugin({
             "ENV": JSON.stringify(METADATA.ENV),
             "HMR": HMR
         })
     ],
+    
+    // Include polyfills or mocks for various node stuff
+    // Description: Node configuration
+    // reference: https://webpack.github.io/docs/configuration.html#node
     node: {
         global: "window",
         //progress: false,
@@ -193,6 +268,10 @@ module.exports = {
         clearImmediate: false,
         setImmediate: false
     },
+        
+    // Static analysis linter for TypeScript advanced options configuration
+    // Description: An extensible linter for the TypeScript language.
+    // reference: https://github.com/wbuchwalter/tslint-loader
     tslint: {
         emitErrors: false,
         failOnHint: false,
@@ -209,9 +288,12 @@ module.exports = {
             browsers: [ "last 2 versions" ]
         })
     ],
-
-    // Other module loader config
-    // our Webpack Development Server config
+    
+    // Webpack Development Server configuration
+    // Description: The webpack-dev-server is a little node.js Express server.
+    // The server emits information about the compilation state to the client,
+    // which reacts to those events.
+    // reference: https://webpack.github.io/docs/webpack-dev-server.html
     devServer: {
         port: METADATA.PORT,
         host: METADATA.HOST,

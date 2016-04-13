3252292156b2f72268cb7240dd7730d2a80d29e0
Sebastien <seb@dsebastien.net>
2016-04-13 22:09:42 +0200

    Import all of Rx

--- src/rxjs-operators.ts
+++ src/rxjs-operators.ts
@@ -1,4 +1,9 @@
 // RxJS operators
 // These are loaded in vendor.ts, but also in the config/spec-bundle.ts file used by Karma
 // Add any additional required operators here
-require("rxjs/add/operator/map");
+
+// load all operators
+require("rxjs/Rx");
+
+// alternative: load operators selectively
+// require("rxjs/add/operator/map");
